67cc30cebe036ae0bea4d5c0819628a05aa44f0e
sohan-chaudhari <sohanchaudhari384240@gmail.com>
2025-02-04 10:05:07 +0530

    Add files via upload

---
+++ loop-10.py
@@ -0,0 +1,15 @@
+#print Fibonacci series till given number
+def numbers(num):
+    if num==1:
+       print(1)
+    if num>=2:
+        print(1,1,end=" ")
+    sum,a,b=0,1,1
+    for i in range(3,num+1):
+        sum=a+b
+        a=b
+        b=sum
+        print(sum,end=" ")
+num=int(input("enter the number :"))
+sum=numbers(num)
+
