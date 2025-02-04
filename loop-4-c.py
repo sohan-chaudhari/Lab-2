6d0b30d445058fff3fad59cea5db501d3ced47f2
sohan-chaudhari <sohanchaudhari384240@gmail.com>
2025-02-04 09:55:12 +0530

    Add files via upload

---
+++ loop-4-c.py
@@ -0,0 +1,14 @@
+def numbers(x):
+    i=0
+    num=str(x)
+    b=len(num)
+    for z in num:
+       i=i+int(z)**b
+    print("the total of each number at power of digits is:",i) 
+    if i==int(x):
+        print("this number is armstrong.")
+    else:
+        print("this number is not armstrong.")
+        
+x=int(input("enter a number : "))
+numbers(x)        
